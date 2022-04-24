15bf0b3d4d551f0702c42d03fcd88daa36346eac
ksushakyky <ksudesignru@gmail.com>
2022-04-24 14:44:29 +0300

    refactoring

--- src/main/java/main/AppController.java
+++ src/main/java/main/AppController.java
@@ -15,7 +15,6 @@ import java.util.Date;
 @RestController
 public class AppController {
 
-<<<<<<< HEAD
     @Autowired
     RestTemplate restTemplate;
 
@@ -61,48 +60,3 @@ public class AppController {
         clientRepository.save((new Client(1,  date.toString(), request.getRequestURL().toString(), request.getRemoteAddr().toString())));
     }
 }
-=======
-        @Autowired
-        RestTemplate restTemplate;
-
-        @Autowired
-        private ClientRepository clientRepository;
-
-        private Bot bot = new Bot();
-
-        @GetMapping("/ping")
-        public ResponseEntity<String> getDetailsPing(Model model, HttpServletRequest request){
-            //        List<Client> list
-            //
-            //            Iterable<Client> clientIterable =  ClientRepository.findAll();
-            //            ArrayList<Client> clients = new ArrayList<>();
-            //            for(Client client : clientIterable) {
-            //                clients.add(client);
-            //            }
-            Date date = new Date();
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("user-agent", "Application");
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<String> entity = new HttpEntity<>(headers);
-            ResponseEntity<String> responseJson = restTemplate.exchange("https://api.catboys.com/ping", HttpMethod.GET, entity, String.class);
-            clientRepository.save((new Client(1,  date.toString(), request.getRequestURL().toString(), request.getRemoteAddr().toString())));
-            bot.sendMsg(responseJson);
-            return responseJson;
-        }
-
-        @GetMapping("/catboy")
-        public ResponseEntity<String> getDetailsCatboy() {
-            Date date = new Date();
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("user-agent", "Application");
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<String> entity = new HttpEntity<>(headers);
-            ResponseEntity<String> responseJson = restTemplate.exchange("https://api.catboys.com/catboy", HttpMethod.GET, entity, String.class);
-            clientRepository.save((new Client(1,  date.toString(), request.getRequestURL().toString(), request.getRemoteAddr().toString())));
-            bot.sendMsg(responseJson);
-            return responseJson;
-        }
-}
->>>>>>> 0adb5b4e16e605636673a1e436371c83ff4b3b19
